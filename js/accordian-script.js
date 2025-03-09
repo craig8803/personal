3f787a6e37bf84e161bd67028fa0918415342b64
craig8803 <106348593+craig8803@users.noreply.github.com>
2025-03-09 12:10:43 +0100

    Add files via upload

--- js/accordian-script.js
+++ js/accordian-script.js
@@ -1,19 +1,10 @@
 <script>
     // Select all accordion buttons
-    const accordions = document.querySelectorAll('.accordion');
-
-accordions.forEach(accordion => {
+    const accordions = document.querySelectorAll('.accordion').forEach(accordion => {
     accordion.addEventListener('click', function() {
-        accordions.forEach(item => {
-            if (item !== this) {
-                item.classList.remove('active');
-                item.nextElementSibling.style.display = "none";
-            }
-        });
-
         this.classList.toggle('active');
-        const panel = this.nextElementSibling;
-        panel.style.display = (panel.style.display === "block") ? "none" : "block";
+        let panel = this.nextElementSibling;
+        panel.style.display = panel.style.display === "block" ? "none" : "block";
     });
 });
 </script>
